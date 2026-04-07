03a116059a185b8e6ebcba9ccdf8e423ed64ff94
Brian Chege <42249736+gichuhibrian@users.noreply.github.com>
2026-04-07 12:50:26 +0300

    feat: Implement Configure PostgreSQL database and environment variables

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,18 @@ module.exports = {
     '**/tests/**/*.spec.js',
     '**/__tests__/**/*.test.js'
   ],
+  transform: {
+    '^.+\.tsx?$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript'
+      ]
+    }]
+  },
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
   collectCoverageFrom: [
     'apps/**/*.{js,ts}',
     '!apps/**/node_modules/**',
