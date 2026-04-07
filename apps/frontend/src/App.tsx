3919e066ac23062b594f6ac701714034a459c2aa
Brian Chege <42249736+gichuhibrian@users.noreply.github.com>
2026-04-07 11:43:37 +0300

    feat: Implement Scaffold React frontend application

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
     <div className="app">
       <h1>Forge Todo</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
